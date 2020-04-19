4d988ee1f727652be2e24f978deaf83d24e1813f
richardalmee <come2fouch@gmail.com>
2020-04-20 00:25:01 +0200

    Set eslint, babel and testing libraries up

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ['codingitwrong'],
+  parser: 'babel-eslint',
+  plugins: ['cypress', 'jest', 'react'],
+  env: {
+    browser: true,
+    'jest/globals': true,
+    'cypress/globals': true,
+  },
+};
---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  state = { foo: 'bar' };
+  render() {
+    return <div className='hello'>Hello World !</div>;
+  }
+}
